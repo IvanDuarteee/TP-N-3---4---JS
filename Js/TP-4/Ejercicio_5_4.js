2d7a4ff098bd838b32e4f534b64442942c9215c4
Ivan Duarte <ivanduartepr@gmail.com>
2023-04-03 16:18:17 -0300

    Se realiza ejercicio_5

--- Js/TP-4/Ejercicio_5_4.js
+++ Js/TP-4/Ejercicio_5_4.js
@@ -1,34 +1,34 @@
 // Ejercicio 5
 
 class Persona {
-    constructor(nombre, edad, dni, sexo, peso, altura, anioNacimiento) {
+    constructor(nombre, edad, dni, sexo, peso, altura, añoNacimiento) {
       this.nombre = nombre;
       this.edad = edad;
       this.dni = dni;
       this.sexo = sexo;
       this.peso = peso;
       this.altura = altura;
-      this.anioNacimiento = anioNacimiento;
+      this.añoNacimiento = añoNacimiento;
     }
   
     mostrarGeneracion() {
       let generacion;
-      if (this.anioNacimiento >= 1901 && this.anioNacimiento <= 1927) {
+      if (this.añoNacimiento >= 1901 && this.añoNacimiento <= 1927) {
         generacion = "Generación de los mayores";
         console.log(`${generacion}: Característico de esta generación es el trabajo duro.`);
-      } else if (this.anioNacimiento >= 1928 && this.anioNacimiento <= 1945) {
+      } else if (this.añoNacimiento >= 1928 && this.añoNacimiento <= 1945) {
         generacion = "Generación silenciosa";
         console.log(`${generacion}: Característico de esta generación es la austeridad.`);
-      } else if (this.anioNacimiento >= 1946 && this.anioNacimiento <= 1964) {
+      } else if (this.añoNacimiento >= 1946 && this.añoNacimiento <= 1964) {
         generacion = "Baby Boomers";
         console.log(`${generacion}: Característico de esta generación es la ambición.`);
-      } else if (this.anioNacimiento >= 1965 && this.anioNacimiento <= 1980) {
+      } else if (this.añoNacimiento >= 1965 && this.añoNacimiento <= 1980) {
         generacion = "Generación X";
         console.log(`${generacion}: Característico de esta generación es la independencia.`);
-      } else if (this.anioNacimiento >= 1981 && this.anioNacimiento <= 1993) {
+      } else if (this.añoNacimiento >= 1981 && this.añoNacimiento <= 1993) {
         generacion = "Generación Y o Millennials";
         console.log(`${generacion}: Característico de esta generación es la tecnología.`);
-      } else if (this.anioNacimiento >= 1994 && this.anioNacimiento <= 2010) {
+      } else if (this.añoNacimiento >= 1994 && this.añoNacimiento <= 2010) {
         generacion = "Generación Z";
         console.log(`${generacion}: Característico de esta generación es la globalización.`);
       } else {
@@ -51,10 +51,23 @@ class Persona {
       console.log(`Sexo: ${this.sexo}`);
       console.log(`Peso: ${this.peso}`);
       console.log(`Altura: ${this.altura}`);
-      console.log(`Año de nacimiento: ${this.anioNacimiento}`);
+      console.log(`Año de nacimiento: ${this.añoNacimiento}`);
     }
   
     generaDNI() {
       this.dni = Math.floor(Math.random() * 100000000);
     }
   }
+  
+  let nombre = prompt("Ingrese su nombre:");
+  let edad = parseInt(prompt("Ingrese su edad:"));
+  let dni = prompt("Ingrese su DNI:");
+  let sexo = prompt("Ingrese su sexo (H hombre, M mujer):");
+  let peso = parseFloat(prompt("Ingrese su peso en kilogramos:"));
+  let altura = parseFloat(prompt("Ingrese su altura en metros:"));
+  let añoNacimiento = parseInt(prompt("Ingrese su año de nacimiento:"));
+  
+  let persona = new Persona(nombre, edad, dni, sexo, peso, altura, añoNacimiento);
+  persona.mostrarGeneracion();
+  persona
+  
